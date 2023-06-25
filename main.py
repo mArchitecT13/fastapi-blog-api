06367e613085b14cab16ce775248317650888304
Milen John Thomas <mjohnthomas@paloaltonetworks.com>
2023-06-25 10:45:54 +0530

    #1 commit

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ app.include_router(authentication.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="debug")
